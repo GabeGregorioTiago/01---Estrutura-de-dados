df24f200811f22b4d64c8ae331ab05a50d25a7a6
GabeGregorioTiago <gabrielgregoriopereiratiago@gmail.com>
2022-06-09 11:04:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let nome = "Gabriel"
+console.log(nome)
+let numero = 12
+console.log(numero)
+let ano = 2022
+console.log(ano)
+let sub = ano-numero
+console.log(sub)
+console.log(ano+numero)
+console.log('Estou mexendo no console.log')
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
